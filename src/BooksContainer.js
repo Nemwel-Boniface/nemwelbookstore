3ea69434a080f8a9d6aaf0f7d848494cd216b220
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-03-24 14:10:44 +0300

    Update refactored project to classbased components

---
+++ src/BooksContainer.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+import './stylesheets/App.css';
+import Navbar from './components/Navbar';
+import Books from './components/Books';
+
+class BooksContainer extends Component {
+  state = {
+    myBooks: [
+      {
+        id: 1,
+        genre: 'Action',
+        bkTitle: 'Book Title',
+        bkAuthor: 'Book Author',
+        comments: 'Comments',
+        remove: 'Remove',
+        edit: "Edit",
+      },
+    ]
+   };
+  render() {
+    return (
+      <div className='App'>
+        <Navbar />
+        <Books myBooks={this.state.myBooks}/>
+      </div>
+    );
+  }
+}
+
+export default BooksContainer;
