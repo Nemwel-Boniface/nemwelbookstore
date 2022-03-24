f08c3a85cca79374820f92db8c78f18e97562389
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-03-24 12:37:02 +0300

    Update include project Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <nav>
+        <h1>Nemwel Bookstore</h1>
+        <ul>
+          <li><a href="#">BOOKS</a></li>
+          <li><a href="#">CATEGORIES</a></li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+export default Navbar;
