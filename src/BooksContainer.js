18e5a297cbc6da377b0314b4a2b59f077a9086a1
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-03-29 17:42:59 +0300

    Update refactored component to functional component

--- src/BooksContainer.js
+++ src/BooksContainer.js
@@ -1,43 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './stylesheets/App.css';
 import Navbar from './components/Navbar';
 import Books from './components/Books';
 import Categories from './components/Categories';
 
-class BooksContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      myBooks: [
-        {
-          id: 1,
-          genre: 'Red pill',
-          bkTitle: 'The Way of Men',
-          bkAuthor: 'Jack Donovan',
-          comments: 'Comments',
-          remove: 'Remove',
-          edit: 'Edit',
-          currentChapter: 'Current chapter',
-          chapter: 'Chapter 6',
-          updateProgress: 'UPDATE PRGRESS',
-        },
-      ],
-    };
-  }
-
-  render() {
-    const { myBooks } = this.state;
-    return (
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Books myBooks={myBooks} />} />
-          <Route exact path="/categories" element={<Categories />} />
-        </Routes>
-      </div>
-    );
-  }
+const BooksContainer = () => {
+  const myBooks = [
+    {
+      id: 1,
+      genre: 'Red pill',
+      bkTitle: 'The Way of Men',
+      bkAuthor: 'Jack Donovan',
+      comments: 'Comments',
+      remove: 'Remove',
+      edit: 'Edit',
+      currentChapter: 'Current chapter',
+      chapter: 'Chapter 6',
+      updateProgress: 'UPDATE PRGRESS',
+    },
+  ];
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Books myBooks={myBooks} />} />
+        <Route exact path="/categories" element={<Categories />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default BooksContainer;
