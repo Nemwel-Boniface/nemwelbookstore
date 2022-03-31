a5364aee5d5a82cbc41402e60805d8af0dea0fd8
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-03-31 09:23:21 +0300

    Update to the books categories list

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -29,11 +29,11 @@ const AddBook = () => {
 
         <select name="books" id="bookgenre" defaultValue="Categories">
           <option value="Categories" selected>Categories</option>
+          <option value="redpill">Red pill</option>
           <option value="Action">Action</option>
           <option value="Thriller">Thriller</option>
           <option value="Horror">Horror</option>
           <option value="Historical">Historical</option>
-          <option value="Romance">Romance</option>
           <option value="Kids">Kids</option>
         </select>
         <button type="submit" className="bookbtn">ADD BOOK</button>
